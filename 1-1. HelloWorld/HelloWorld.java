1aa498a242daaed860de48f60349ee340d06ae59
Xinan <youkingan11@gmail.com>
2023-12-29 16:09:52 +0000

    first commit

---
+++ 1-1. HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+// In Java, write all code in a class
+// The class name needs to always match the file name
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        
+        // All class needs a main() method, it is the entry point where Java runs your code
+        System.out.println("Hello World!");
+
+        // Semi-colon ; means end of statement, every single line of java code needs a ;
+        // println(""); prints a message to the console
+        
+    }
+
+}
---
+++ Git-Commands.md
@@ -0,0 +1,16 @@
+### Git Commands:<br>
+* Initialise Git in local folder:<br>
+```git init```
+<br>
+* Link Your Local Repository to GitHub:<br>
+```git remote add origin https://github.com/username/repositoryname.git```
+<br>
+*Stage Your Changes:<br>
+```git add .```
+<br>
+*Commit Your Changes:<br>
+```commit -m "Initial commit".```
+<br>
+*Push to GitHub:<br>
+```git push -u origin master```<br>
+(or main if your default branch is named 'main').
