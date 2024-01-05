bd960cd8ae3409f873d20240202a56331857acde
Xinan <youkingan11@gmail.com>
2024-01-05 16:50:02 +0000

    add calculation

---
+++ 1-4 Calculation/Divide.java
@@ -0,0 +1,14 @@
+public class Divide {
+
+    public static void main(String[] args) {
+        
+        // int
+        System.out.println(4/2); // output: 2
+        System.out.println(5/2); // output: 2
+
+        // float
+        System.out.println(4/2.0); // output: 2.0
+        System.out.println(10/3.0); // output: 3.3333333333333335
+    }
+    
+}
---
+++ 1-4 Calculation/Rounding.java
@@ -0,0 +1,19 @@
+public class Rounding {
+
+    public static void main(String[] args) {
+
+        // Rounding
+
+        System.out.println(Math.floor(3.7)); // Rounds down
+
+        System.out.println(Math.ceil(3.2)); // Rounds up
+
+        System.out.println(Math.ceil(3.5)); // rounding off.
+
+        // Modulus (Finding the Remainder)
+
+        System.out.println(7 % 3); // 7 divided by 3 is 2 with a remainder of 1.
+
+    }
+
+}
